9fd8f21ba57055a100bc6fb9cf13bcfe956325a2
anandtechhh@gmail.com Anand Rathod <anandtechhh@gmail.com>
2023-09-11 19:57:35 +0530

    added gameapp

---
+++ android/app/src/main/kotlin/com/example/gamapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gamapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import '../provider/quiz_provider.dart';
+import 'package:provider/provider.dart';
+
+import 'Admin/AdminDashboard.dart';
+import 'SelectionScreen/selectionScreen.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => QuizProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'GameApp',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.red,
+        ),
+        home: SelectionScreen(),
+        // home: DashBoard(),
+      ),
+    );
+  }
+}
